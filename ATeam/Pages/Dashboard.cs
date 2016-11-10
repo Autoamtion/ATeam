f38a310c89faa38fd8affbcfe157f6247d193c34
Automation <automation@axadirect-solutions.pl>
2016-11-10 16:59:36 +0100

    Menu links: dashboard, registration, sessions, products and profile

---
+++ ATeam/Pages/Dashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace ATeam.Pages
+{
+    public class Dashboard : Page
+    {
+        public Dashboard(IWebDriver webdriver)
+            : base(webdriver)
+        {
+        }
+
+        [FindsBy(How = How.LinkText, Using = "Dziś")]
+        public IWebElement TodayLink { get; set; }
+    }
+}
