f2ef4dbc7745033a723e805dfa0173e54d7c8bf8
Automation <Automation@axadirect-solutions.pl>
2016-11-17 02:53:22 +0100

    ProductDetails, ProductList Mapping

---
+++ ATeam/Pages/Products/List.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATeam.Pages.Products
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.PageObjects;
+
+    public class List : Page
+    {
+       public List(IWebDriver driver) : base(driver)
+       {
+           
+       }
+        [FindsBy(How = How.CssSelector, Using = "input[id*='search']")]
+        public IWebElement SearchField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "table[id*='DataTables_Table_0']>tbody>tr")]
+        public List<IWebElement> ProductList { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "a[href*='/ateam/Products/AddProduct']")]
+        public IWebElement AddProductBtn { get; set; }
+        //TOOD:
+        ////public void GetProducts()
+        ////{
+        ////    foreach (var item in ProductList)
+        ////    {
+        ////        item.FindElement(By.CssSelector(""))
+        ////    }
+        ////    this.ProductList
+        ////}
+    }
+}
---
+++ ATeam/Pages/Products/ProductDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATeam.Pages.Products
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.PageObjects;
+
+    public class ProductDetails : Page
+    {
+
+       public ProductDetails(IWebDriver driver) : base(driver)
+       {
+           
+       }
+        [FindsBy(How = How.Id, Using = "ProductDetails_IsActive")]
+        public IWebElement IsActive { get; set; }
+        [FindsBy(How = How.CssSelector, Using = "[class*='product-delete']")]
+        public IWebElement DeleteProductBtn { get; set; }
+        [FindsBy(How = How.CssSelector, Using = "a[href*='/ateam/Products/EditProduct/']")]
+        public IWebElement EditProductBtn { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "js-product-status")]
+        public IWebElement ProductStatus { get; set; }
+    }
+}
