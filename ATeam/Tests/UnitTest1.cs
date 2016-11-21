1a61d65075e63446d8175b236d77ef2163d96904
Automation <automation@axadirect-solutions.pl>
2016-11-21 11:08:33 +0100

    UnitTest class for ClosedRegistration

---
+++ ATeam/Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ATeam.Tests
+{
+    [TestClass]
+    public class ClosedRegistrationTest : BaseTest
+    {
+        [TestMethod]
+        public void ClosedRegistrationWithSpecificLocation()
+        {
+        }
+    }
+}
